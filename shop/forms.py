29526b51cd891b6281a35194a650b3fda76810df
Olzhas <olzhas.koshkarbay@gmail.com>
2024-06-01 18:16:43 +0600

    r

---
+++ shop/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class AddToCartForm(forms.Form):
+    quantity = forms.IntegerField(min_value=1, initial=1)
+    product_id = forms.IntegerField(widget=forms.HiddenInput)
+
+    
--- shop/models.py
+++ shop/models.py
@@ -6,3 +6,16 @@ class Shop(models.Model):
     quantity = models.IntegerField()
     image = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+    
+class Cart(models.Model):
+    items = models.ManyToManyField(CartItem)
+    
+    def __str__(self):
+        return f"Cart with {self.items.count()} items"
